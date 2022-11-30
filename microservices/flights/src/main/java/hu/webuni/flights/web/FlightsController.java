86f84e5737b9ef9c119c1d6decb9d0182c78fa85
patonaiszabolcs <patonai.szabolcs@gmail.com>
2022-11-30 21:26:04 +0100

    Week 7- Securing flights  method level

--- microservices/flights/src/main/java/hu/webuni/flights/web/FlightsController.java
+++ microservices/flights/src/main/java/hu/webuni/flights/web/FlightsController.java
@@ -6,6 +6,8 @@ import hu.webuni.flights.service.AirlineService;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +17,8 @@ public class FlightsController implements FlightsApi {
 
     @Autowired
     private AirlineService airlineService;
-    
+
+    @PreAuthorize("hasAuthority('search')")
     @Override
     public List<Airline> searchFlight(String from, String to) {
         return airlineService.search(from, to);
